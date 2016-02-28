c2068754c5bdb08d73e507f82d9279e50368a74a
Abraham Kiggundu <abraham@thekiggys.com>
2016-02-28 17:03:29 +0000

    implemented service to return array of primes

--- server/api/primes/primes.model.js
+++ server/api/primes/primes.model.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 const readFile = require('fs-readfile-promise');
 const path = require('path');
+const primesJson = require(path.resolve(__dirname, 'data/1000.primes.json'))
 
 var model = function() {
 
   return {
     getPrimes: function(){
-      console.log('dirname: ' + path.resolve(__dirname, 'data/1000.txt'));
-      return readFile(path.resolve(__dirname, 'data/1000.txt'));
+      return primesJson;
     }
   }
 }
--- server/api/primes/primes.model.unit.spec.js
+++ server/api/primes/primes.model.unit.spec.js
@@ -1,18 +1,11 @@
 var primes = require('./primes.model');
 
-describe("Primes model", function (done) {
-  it("returns a list of primes", function (done) {
-    primes.getPrimes()
-      .then(function (primeList) {
-        console.log('Cool: ' + primeList);
-        //expect(primeList).toEqual("");
-        done();
-      })
-    .catch(function(err){
-     console.log('Something bad happened: ' + err);
-        console.log('Error getting file: ' + err);
-        //expect(true).toEqual(false);
-        done();
-      })
+describe("Primes model", function () {
+  it("returns an array of primes", function () {
+    var primeNumbers = primes.getPrimes();
+    expect(primeNumbers).to.be.instanceof(Array);
+    assert.instanceOf(primeNumbers, Array);
+    assert.include(primeNumbers, 29);
+
   })
 })
