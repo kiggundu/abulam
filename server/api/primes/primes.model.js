25abb6d5b38c0cb4cbac4853a79115c182451cac
Abraham Kiggundu <abraham@thekiggys.com>
2016-03-06 00:24:19 +0000

    Added RX streaming for primes

--- server/api/primes/primes.model.js
+++ server/api/primes/primes.model.js
@@ -1,15 +1,68 @@
 const fs = require('fs');
 const readFile = require('fs-readfile-promise');
 const path = require('path');
-const primesJson = require(path.resolve(__dirname, 'data/1000.primes.json'))
+var Rx = require('rx');
+var makeSource = require("stream-json");
+var log4js = require('log4js');
+var scriptName = path.basename(__filename);
+var logger = log4js.getLogger(scriptName);
+logger.setLevel("INFO");
 
-var model = function() {
+
+var model = function () {
+
 
   return {
-    getPrimes: function(){
-      return primesJson;
+
+    fromJsonFile: function (filePath) {
+      const primesJson = require(filePath);
+      //var primesBufferSize = 10;
+      var primesStream = Rx.Observable
+        .from(primesJson)
+        //.bufferWithCount(primesBufferSize);
+      return primesStream;
+    },
+
+    fromJsonFileStream: function (filePath) {
+
+      var primesObservable = Rx.Observable.create(function(observer){
+        var fs = require("fs");
+        var source = makeSource();
+        var objectCounter = 0;
+        var currentNumber = "";
+
+        source.on("startNumber", function () {
+          logger.debug("Found number...");
+          ++objectCounter;
+        });
+
+        source.on("numberChunk", function (chunk) {
+          currentNumber = currentNumber+chunk;;
+        });
+
+        source.on("endNumber", function () {
+          logger.debug("number is: ", currentNumber);
+          observer.onNext(parseInt(currentNumber));
+          currentNumber = "";
+        });
+
+        source.on("end", function () {
+          logger.info("Found ", objectCounter, " numbers.");
+          observer.onCompleted()
+        });
+
+        fs.createReadStream(filePath).pipe(source.input);
+
+      });
+
+      return primesObservable;
+    },
+
+    fromJson: function (primesJson) {
+      return getPrimesObservable(primesJson);
     }
   }
-}
+};
 
-export default model()
+
+export default model();
--- server/api/primes/primes.model.unit.spec.js
+++ server/api/primes/primes.model.unit.spec.js
@@ -1,11 +1,36 @@
-var primes = require('./primes.model');
+const path = require('path');
+var log4js = require('log4js');
+var scriptName = path.basename(__filename);
+var Rx = require('rx');
+var logger = log4js.getLogger(scriptName);
+logger.setLevel("INFO");
 
-describe("Primes model", function () {
-  it("returns an array of primes", function () {
-    var primeNumbers = primes.getPrimes();
-    expect(primeNumbers).to.be.instanceof(Array);
-    assert.instanceOf(primeNumbers, Array);
-    assert.include(primeNumbers, 29);
+describe("Primes model", function(){
+
+
+  it("returns an array of primes", function (done) {
+    console.time("Primes test");
+    //var primesObservable = require('./primes.model').fromJsonFileStream(path.resolve(__dirname, 'data/1000.primes.json'));
+    var primesObservable = require('./primes.model').fromJsonFile(path.resolve(__dirname, 'data/1000.primes.json'));
+    var primesCounter = 0;
+
+    var observer = Rx.Observer.create(
+      function(next){
+        logger.debug("Value obtained: " + next);
+        primesCounter++;
+      },
+      function(error){
+        logger.error("Error obtained: " + error);
+        expect(true).to.equal(false);
+      },
+      function(){
+        expect(primesCounter).to.equal(1000);
+        logger.info("Completed");
+        console.timeEnd("Primes test");
+        done();
+      }
+    );
+    primesObservable.subscribe(observer);
 
   })
 })
